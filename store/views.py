97d7fb370543cc02625dcedad43a1cbb0df570d4
Steve Nguyen <lunarfox24@yahoo.com>
2021-06-07 15:21:43 -0700

    new commit

--- store/views.py
+++ store/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse 
 from .models import *
+
 
 def store(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store/store.html', context)
 
+
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
-    context = {'items':items, 'order':order}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
+
 
 def checkout(request):
     if request.user.is_authenticated:
@@ -26,7 +31,11 @@ def checkout(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
-    context = {'items':items, 'order':order}
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.data)
+    return JsonResponse('Item was added', safe=False)
